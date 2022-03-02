02848d3f67646738af9624b6c7c8411c4f770714
AriParvin <arian.parvin@parvin.de>
2022-03-02 22:57:52 +0100

    added home component and navbar

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BriefingCardsComponent } from './expenses/briefing-cards/briefing-cards.component';
 import { ExpenseListComponent } from './expenses/expense-list/expense-list.component';
+import { HomeComponent } from './home/home.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     BriefingCardsComponent,
-    ExpenseListComponent
+    ExpenseListComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
