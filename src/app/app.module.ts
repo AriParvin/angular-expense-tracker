bdd623572db06e62a6783ec04389d793ffe97d5b
AriParvin <arian.parvin@parvin.de>
2022-03-03 00:40:36 +0100

    added data binding and structural directives. error TS2339 (Property 'x' does not exist on type 'y') unresolved.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { CommonModule } from '@angular/common';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -12,13 +13,10 @@ import { HomeComponent } from './home/home.component';
     AppComponent,
     BriefingCardsComponent,
     ExpenseListComponent,
-    HomeComponent
+    HomeComponent,
   ],
-  imports: [
-    BrowserModule,
-    AppRoutingModule
-  ],
+  imports: [BrowserModule, AppRoutingModule, CommonModule],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-export class AppModule { }
+export class AppModule {}
---
+++ src/app/home/home.component.ts
@@ -0,0 +1,32 @@
+import { Component, OnInit } from '@angular/core';
+import IExpense from '../expenses/expense';
+
+@Component({
+  selector: 'et-home',
+  templateUrl: './home.component.html',
+  styleUrls: ['./home.component.css'],
+})
+export class HomeComponent implements OnInit {
+  constructor() {}
+  expenses: IExpense[] = [
+    {
+      description: 'First shopping for the month',
+      amount: 20,
+      date: '2019-08-12',
+    },
+    {
+      description: 'Bicycle for Amy',
+      amount: 10,
+      date: '2019-08-08',
+    },
+    {
+      description: 'First shopping for the month',
+      amount: 14,
+      date: '2019-08-21',
+    },
+  ];
+  currentMonthSpending = { amount: 300, month: 'July' };
+  lastMonthSpending = { amount: 44, month: 'August' };
+
+  ngOnInit() {}
+}
